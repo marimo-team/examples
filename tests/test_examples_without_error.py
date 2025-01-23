3197010e64f61f1f83ce0163114b3de5c33d6cc0
Akshay Agrawal <akshaykagrawal7@gmail.com>
2025-01-22 22:17:42 -0800

    smoke test embed

--- tests/test_examples_without_error.py
+++ tests/test_examples_without_error.py
@@ -5,3 +5,9 @@ def test_nlp_span_comparison() -> None:
     from nlp_span_comparison import nlp_span_comparison
 
     assert not nlp_span_comparison.app._unparsable
+
+
+def test_explore_high_dimensional_data() -> None:
+    from explore_high_dimensional_data import explore_high_dimensional_data
+
+    assert not explore_high_dimensional_data.app._unparsable
